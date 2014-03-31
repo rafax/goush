abdbb8765983832fd7c27946ea08cbe0bd3ba056
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2014-03-31 22:06:23 +0200

    Connect to redis

--- goush.go
+++ goush.go
@@ -1,11 +1,19 @@
 package main
 
 import "github.com/go-martini/martini"
+import "github.com/garyburd/redigo/redis"
 
 func main() {
+	c, err := redis.Dial("tcp", ":6379")
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close()
+
 	m := martini.Classic()
 	m.Get("/", func() string {
-		return "Hello world!"
+		url, _ := redis.String(c.Do("GET", "u:1"))
+		return "Hello rafał! " + url
 	})
 	m.Run()
 }
